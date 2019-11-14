3629a12f85bd05d71475821ba83473db2b7f1d56
GoodWinXx <709168832@qq.com>
2019-11-14 21:02:26 +0800

    11-14更新

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -29,39 +29,14 @@ class Login extends Controller
                 dump($data);exit;
             }else{
                 $user = Db::name('admin_user')->where('admin_name','=',input('admin_name'))->find();
-                session('user',$user);
-                if (md5($user['password']) == md5(input('password'))){
+                if ($user['password'] == md5(input('password'))){
+                    session('user',$user);
                     return $this->success('登陆成功!','admin/index');
                 }elseif (md5($user['password']) != md5(input('password'))){
                     return $this->error('账号或密码错误','login');
-                }else{
-                    return $this->error('用户不存在','login');
                 }
             }
         }
         return $this->fetch();
-/*        if (request()->isPost()){
-            $data = $this->validate(
-                [
-                    'username' => input('username'),
-//                    'username' => "",
-                ],
-                'app\admin\validate\Admin');
-//            dump($data);exit;
-            if (true !== $data) {
-                // 验证失败 输出错误信息
-                dump($data);
-            }else{
-                $user = Db::name('admin')->where('username','=',input('username'))->find();
-                if (md5($user['password']) == md5(input('password'))){
-                    return $this->success('login success!','admin/index');
-                }elseif (md5($user['password']) != md5(input('password'))){
-                    return $this->error('账号或密码错误','login');
-                }else{
-                    return $this->error('用户不存在','login');
-                }
-            }
-        }
-        return $this->fetch();*/
     }
 }
